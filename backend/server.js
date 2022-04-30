cc636734b4a5e06a1505fa58ea4196853b3536f7
Seham <wisdom150@hotmail.com>
2022-04-30 05:20:23 +0300

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB= require('./config/db')
+const port = process.env.port || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
